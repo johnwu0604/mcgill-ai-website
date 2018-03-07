a0e08d26c2748fbaf72bc1d439405c77fa727dd5
John Wu <john.wu@mail.mcgill.ca>
2018-03-07 14:26:14 -0500

    Email is fully working

---
+++ public/assets/js/angular/core.js
@@ -0,0 +1,25 @@
+var app = angular.module('mcgill-ai', [])
+
+app.controller('mainController', function($scope, $http) {
+
+    $scope.loading = true;
+    $scope.emailFormData = {};
+    $scope.email_success_message = ''
+    $scope.result = ''
+
+    $scope.sendEmail = function() {
+        if (!$scope.emailFormData.email || !$scope.emailFormData.name || !$scope.emailFormData.message) {
+            $scope.email_success_message = 'It looks like you have some missing fields. Please fill out all fields on the form.'
+        } else {
+            $http.post('/email', $scope.emailFormData).success(function(result) {
+                $scope.emailFormData = {}
+                if (result.success) {
+                    $scope.email_success_message = 'Your email was successfully sent!'
+                } else {
+                    $scope.email_success_message = 'Seems like our server is not working correctly. Please contact us through Facebook.'
+                }
+            })
+        }
+    }
+
+});
--- routes/email.js
+++ routes/email.js
@@ -1,7 +1,38 @@
+var request = require('request')
+
 module.exports = function(app) {
-    
-    // TODO: IMPLEMENT EMAIL SERVICE
-    app.get('/email', function(req, res) {
-        res.send('Email');
+
+    app.post('/email', function(req, res) {
+        // Configure the request for sending to slack
+        var options = {
+            url: 'https://hooks.slack.com/services/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',
+            method: 'POST',
+            headers: {
+                'User-Agent':       'Super Agent/0.0.1',
+                'Content-Type':     'application/json'
+            },
+            json: {
+                'text': 'New email received from McGill AI Society Website!',
+                'attachments': [
+                    {
+                        'title': req.body.name + ' (' + req.body.email + ') ',
+                        'text': req.body.message
+                    }
+                ]
+            }
+        }
+        // Process the request
+        request(options, function (error, response, body) {
+            if (error) {
+                console.log(error)
+                res.send({
+                    'success': false
+                })
+            }
+            res.send({
+                'success': true
+            })
+        })
     })
+
 }
